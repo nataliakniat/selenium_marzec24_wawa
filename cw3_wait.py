757a849bdb103aa3bd7970430f668e101b45f843
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-09 13:17:33 +0100

    wait ciag dalszy

--- cw3_wait.py
+++ cw3_wait.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
+
+# implicitly wait - oczekiwanie na pojawienie się każdego elementu na stronie przez maksymalnie X sekund
+# driver.implicitly_wait(10)
 
 driver.get("https://www.w3schools.com/")
 driver.maximize_window()
@@ -23,6 +29,17 @@ webdriver.ActionChains(driver).move_to_element(menu).click().perform()
 # po xpath pisanym 'z palca'
 learnHTML = driver.find_element("xpath", "//a[@title='HTML Tutorial']")
 learnHTML.click()
+
+# explicity wait - oczekiwanie na ten jeden konkretny element
+wait = WebDriverWait(driver, 10, 0.5)
+# warunek z dostępnej biblioteki
+#wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='leftmenuinnerinner']/a[43]")))
+# warunek własny
+wait.until(lambda x: len(x.find_elements("xpath", "//*[@id='leftmenuinnerinner']/a[430]")))
+
+# wybranie menu 'Input types'
+menuInput = driver.find_element("xpath", "//*[@id='leftmenuinnerinner']/a[43]")
+menuInput.click()
 
 time.sleep(600)
 driver.quit()
