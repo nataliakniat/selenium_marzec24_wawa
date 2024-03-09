608e501c48a32b2d6218b84d093dffef215cdbd6
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-09 12:44:42 +0100

    update

--- cw3_wait.py
+++ cw3_wait.py
@@ -11,7 +11,18 @@ driver.find_element("id","accept-choices").click()
 
 # wybranie menu "Tutorials"
 menu = driver.find_element("id", "navbtn_tutorials")
-menu.click()
+# menu.click()
+
+# klikanie za pomoca akcji
+# webdriver.ActionChains(driver).move_to_element(menu).perform()
+webdriver.ActionChains(driver).move_to_element(menu).click().perform()
+
+# wybranie tutoriala 'learn HTML'
+# po xpath skopiowanym z narzedzi developerskich
+#learnHTML = driver.find_element("xpath", "//*[@id='tutorials_html_css_links_list']/div[1]/a[1]")
+# po xpath pisanym 'z palca'
+learnHTML = driver.find_element("xpath", "//a[@title='HTML Tutorial']")
+learnHTML.click()
 
 time.sleep(600)
 driver.quit()
