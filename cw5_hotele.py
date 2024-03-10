28783175011dfe73d5bce33c2d30563f75bbde34
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-10 09:26:55 +0100

    Wyszukiwanie

--- cw5_hotele.py
+++ cw5_hotele.py
@@ -7,6 +7,24 @@ driver.maximize_window()
 
 driver.get("http://www.kurs-selenium.pl/")
 
+# wprowadzenie nazwy miasta
+driver.find_element("xpath", "//span[text()='Search by Hotel or City Name']").click()
+driver.find_element("xpath", "//*[@id='select2-drop']/div/input").send_keys("Dubai")
+driver.find_element("xpath", "//span[text()='Dubai']").click()
+
+# wybranie daty
+driver.find_element("name", "checkin").send_keys("10/03/2024")
+driver.find_element("name", "checkout").send_keys("12/03/2024")
+
+# wybranie liczby turystów
+driver.find_element("id", "travellersInput").click()
+driver.find_element("id", "adultInput").clear()
+driver.find_element("id", "adultInput").send_keys("1")
+driver.find_element("id", "childInput").clear()
+driver.find_element("id", "childInput").send_keys("2")
+
+# wyszukaj
+driver.find_element("xpath", "//*[@id='hotels']/form/div[5]/button").click()
 
 time.sleep(500)
 driver.quit()
