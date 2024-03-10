2d1332f0a24e42cefc1f5edc508c47a8574fafe4
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-10 13:51:33 +0100

    logowanie update

--- page_object_pattern/pages/search_hotel.py
+++ page_object_pattern/pages/search_hotel.py
@@ -1,9 +1,11 @@
+import logging
 from page_object_pattern.locators.locators import SearchHotelLocators
 
 class SearchHotelPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger(__name__)
         self.search_hotel_span_xpath = SearchHotelLocators.search_hotel_span_xpath
         self.search_hotel_input_xpath = SearchHotelLocators.search_hotel_input_xpath
         self.search_hotel_match_xpath = SearchHotelLocators.search_hotel_match_xpath
@@ -15,15 +17,18 @@ class SearchHotelPage:
         self.search_button_xpath = SearchHotelLocators.search_button_xpath
 
     def set_city(self, city):
+        self.logger.info("Setting city {}".format(city))
         self.driver.find_element("xpath", self.search_hotel_span_xpath).click()
         self.driver.find_element("xpath", self.search_hotel_input_xpath).send_keys(city)
         self.driver.find_element("xpath", self.search_hotel_match_xpath).click()
 
     def set_date_range(self, checkin, checkout):
+        self.logger.info("Setting date range. Checkin: {checkin} and checkout: {checkout}".format(checkin=checkin, checkout= checkout))
         self.driver.find_element("name", self.checkin_input_name).send_keys(checkin)
         self.driver.find_element("name", self.checkout_input_name).send_keys(checkout)
 
     def set_travellers(self, adult, kids):
+        self.logger.info("Setting travellers adults: {adults} and kids {kids}".format(adults=adult, kids=kids))
         self.driver.find_element("id", self.travellers_input_id).click()
         self.driver.find_element("id", self.adult_input_id).clear()
         self.driver.find_element("id", self.adult_input_id).send_keys(adult)
@@ -31,4 +36,5 @@ class SearchHotelPage:
         self.driver.find_element("id", self.child_input_id).send_keys(kids)
 
     def perform_search(self):
+        self.logger.info("I'm searching :)")
         self.driver.find_element("xpath", self.search_button_xpath).click()
--- page_object_pattern/pages/search_result.py
+++ page_object_pattern/pages/search_result.py
@@ -1,3 +1,4 @@
+import logging
 from page_object_pattern.locators.locators import SearchResultLocators
 
 
@@ -5,12 +6,16 @@ class SearchResultPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger(__name__)
         self.hotel_names_xpath = SearchResultLocators.hotel_names_xpath
         # TO DO: dodac atrybut do wyswietlenia cen
 
     def get_hotels_name(self):
         hotels = self.driver.find_elements("xpath", self.hotel_names_xpath)
         hotels_name = [hotel.get_attribute("textContent") for hotel in hotels]
+        self.logger.info("Avialable hotels:")
+        for name in hotels_name:
+            self.logger.info(name)
         return hotels_name
 
     # TO DO: metoda do pobierania cen
